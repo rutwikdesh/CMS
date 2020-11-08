939e3ab73415d8d52085617208c9a24fdfa489be
Rutwik Deshmukh <47482137+rutwikdesh@users.noreply.github.com>
2020-11-08 22:54:13 +0530

    Update mysql_database.sql

--- mysql_database.sql
+++ mysql_database.sql
@@ -1,32 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 31, 2019 at 01:37 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `canteen`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
 
 CREATE TABLE `products` (
   `product_id` int(3) NOT NULL,
@@ -38,10 +13,6 @@ CREATE TABLE `products` (
   `product_start_time` int(2) NOT NULL,
   `product_end_time` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `products`
---
 
 INSERT INTO `products` (`product_id`, `product_name`, `product_price`, `product_quantity`, `product_type`, `product_state`, `product_start_time`, `product_end_time`) VALUES
 (3, 'Idly(2 nos.)', 30, 10, 1, 1, 7, 10),
@@ -65,12 +36,6 @@ INSERT INTO `products` (`product_id`, `product_name`, `product_price`, `product_
 (21, 'Chocolate Cake', 20, 13, 1, 1, 16, 18),
 (22, 'Sandwich', 25, 20, 1, 1, 15, 17);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `sales`
---
-
 CREATE TABLE `sales` (
   `sale_id` int(3) NOT NULL,
   `product_id` int(3) NOT NULL,
@@ -82,21 +47,11 @@ CREATE TABLE `sales` (
   `order_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `sales`
---
-
 INSERT INTO `sales` (`sale_id`, `product_id`, `product_name`, `customer_id`, `product_quantity`, `product_price`, `amount_paid`, `order_date`) VALUES
 (2, 2, 'Burger', 13, 6, 50, 300, '2000-03-12'),
 (3, 2, 'Burger', 13, 1, 50, 50, '2000-03-12'),
 (4, 2, 'Burger', 13, 3, 50, 150, '2000-07-26'),
 (5, 2, 'Burger', 13, 3, 50, 150, '2003-07-23');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `user_id` int(3) NOT NULL,
@@ -110,51 +65,26 @@ CREATE TABLE `users` (
   `user_wallet_balance` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
-
 INSERT INTO `users` (`user_id`, `user_name`, `user_password`, `user_type`, `user_first_name`, `user_last_name`, `user_age`, `user_address`, `user_wallet_balance`) VALUES
 (10, 'administrator', 'password', 0, 'Administrator', 'Admin', 18, '5th Avenue', 0),
 (14, 'arjun', 'password', 1, 'Arjun', 'Ashok', 18, 'ABCD Ramnagar', 100),
 (15, 'baalavignesh', 'password', 1, 'Baalavignesh', 'A', 18, 'ZXCVBNM', 100),
 (16, 'rajesh', 'cplusplus', 1, 'Rajesh', 'Guru', 23, 'Road 1, Cbe', 100);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `products`
---
 ALTER TABLE `products`
   ADD PRIMARY KEY (`product_id`);
 
---
--- Indexes for table `sales`
---
 ALTER TABLE `sales`
   ADD PRIMARY KEY (`sale_id`);
 
---
--- Indexes for table `users`
---
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `products`
---
 ALTER TABLE `products`
   MODIFY `product_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
---
--- AUTO_INCREMENT for table `sales`
---
 ALTER TABLE `sales`
   MODIFY `sale_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
@@ -164,7 +94,3 @@ ALTER TABLE `sales`
 ALTER TABLE `users`
   MODIFY `user_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
